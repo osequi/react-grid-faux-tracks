586d328af6c9b52926519f8809dc81300818bbf9
osequi <cs@osequi.com>
2020-09-19 08:53:07 +0300

    Adding Storybook tests

--- src/components/GridFauxLines/GridFauxLines.stories.js
+++ src/components/GridFauxLines/GridFauxLines.stories.js
@@ -1,9 +1,50 @@
 import React from "react";
-import { storiesOf } from "@storybook/react";
-
 import GridFauxLines from "./GridFauxLines";
-import description from "./GridFauxLines.md";
 
-storiesOf("GridFauxLines", module).add("Overview", () => <GridFauxLines />, {
-  notes: { markdown: description }
-});
+export default {
+  title: "Componens/GridFauxLines",
+  component: GridFauxLines,
+};
+
+const Template = (args) => <GridFauxLines {...args} />;
+
+export const Default = Template.bind({});
+Default.decorators = [
+  (Story) => (
+    <div>
+      <p>It should be empty because `children` is not set by default.</p>
+      <Story />
+    </div>
+  ),
+];
+
+export const WithChildren = Template.bind({});
+WithChildren.args = { children: "children" };
+WithChildren.decorators = [
+  (Story) => (
+    <div>
+      <p>
+        It should be empty because `rows`, `columns` are not set by default.
+      </p>
+      <Story />
+    </div>
+  ),
+];
+
+export const WithChildrenColsAndRows = Template.bind({});
+WithChildrenColsAndRows.args = { ...WithChildren.args, columns: 3, rows: 3 };
+WithChildrenColsAndRows.decorators = [
+  (Story) => (
+    <div>
+      <p>It should be empty because `lines` are not set by default.</p>
+      <Story />
+    </div>
+  ),
+];
+
+export const WithAllPropsSet = Template.bind({});
+WithAllPropsSet.args = {
+  ...WithChildrenColsAndRows.args,
+  lines: "both",
+  children: "Displays no grid since children is just a string",
+};
